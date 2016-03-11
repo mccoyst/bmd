ea22249da82d713429b3de0e866674ac21d67c2d
Steve McCoy <mccoyst@gmail.com>
2016-03-11 18:20:40 -0500

    Tutorial part two done

--- main.js
+++ main.js
@@ -1,5 +1,54 @@
+var keymap = {};
+keymap[38] = 0;
+keymap[33] = 1;
+keymap[39] = 2;
+keymap[34] = 3;
+keymap[40] = 4;
+keymap[35] = 5;
+keymap[37] = 6;
+keymap[36] = 7;
+
+var Player = function(x, y){
+	this.x = x;
+	this.y = y;
+};
+
+Player.prototype.draw = function(){
+	Game.display.draw(this.x, this.y, "@", "#f00");
+};
+
+Player.prototype.act = function(){
+	Game.engine.lock();
+	window.addEventListener("keydown", this);
+};
+
+Player.prototype.handleEvent = function(e){
+	var c = e.keyCode;
+	if(!(c in keymap)){
+		return;
+	}
+
+	var diff = ROT.DIRS[8][keymap[c]];
+	var x1 = this.x + diff[0];
+	var y1 = this.y + diff[1];
+	var nk = x1 + "," + y1;
+	if(!(nk in Game.map)){
+		return;
+	}
+
+	// Draw what was beneath the player:
+	Game.display.draw(this.x, this.y, Game.map[this.x+","+this.y])
+	this.x = x1;
+	this.y = y1;
+	this.draw();
+	window.removeEventListener("keydown", this);
+	Game.engine.unlock();
+};
+
 var Game = {
+	engine: null,
 	display: null,
+	player: null,
 	map: {},
 	colors: {
 		'.': '#fff',
@@ -10,6 +59,10 @@ var Game = {
 		this.display = new ROT.Display();
 		document.body.appendChild(this.display.getContainer());
 		this.generateMap();
+		var sched = new ROT.Scheduler.Simple();
+		sched.add(this.player, true);
+		this.engine = new ROT.Engine(sched);
+		this.engine.start();
 	},
 
 	generateMap: function(){
@@ -25,16 +78,24 @@ var Game = {
 			this.map[key] = ".";
 		};
 		digger.create(store.bind(this));
-		this.generateBoxes(cells);
+		cells = cells.randomize();
+		this.generateBoxes(cells.slice(0,10));
+		this.generatePlayer(cells.slice(10,11));
 		this.drawWholeMap();
+		this.player.draw();
 	},
 
 	generateBoxes: function(cells){
-		cells = cells.randomize();
 		for(var i = 0; i < 10; i++){
-			console.log(cells[i]);
 			this.map[cells[i]] = "*";
 		}
+	},
+
+	generatePlayer: function(cells){
+		var parts = cells[0].split(",");
+		var x = parseInt(parts[0]);
+		var y = parseInt(parts[1]);
+		this.player = new Player(x, y);
 	},
 
 	drawWholeMap: function(){
@@ -46,3 +107,4 @@ var Game = {
 		}
 	},
 };
+
